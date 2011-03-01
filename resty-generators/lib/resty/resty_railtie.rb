653d96ef6be7de5d750db9918b5ebaf7cd8611b0
kristian <m.kristian@web.de>
2011-03-01 20:12:30 +0530

    fix the time/date formats match the one resty uses

--- resty-generators/lib/resty/resty_railtie.rb
+++ resty-generators/lib/resty/resty_railtie.rb
@@ -12,5 +12,33 @@ module Resty
       end
       #Erb::Generators::ScaffoldGenerator.source_paths.insert(0, File.expand_path('../../generators/ixtlan/templates', __FILE__))
     end
+
+    config.after_initialize do
+      ActiveRecord::Base.include_root_in_json = false
+      # TODO there migt be a way to tell ALL ActiveModel:
+      #ActiveModel::Base.include_root_in_json = false
+
+      # get the time/date format right ;-) and match it with resty
+      class DateTime
+        def as_json(options = nil)
+          strftime('%Y-%m-%dT%H:%M:%S.%s%z')
+        end
+      end
+      class ActiveSupport::TimeWithZone
+        def as_json(options = nil)
+          strftime('%Y-%m-%dT%H:%M:%S.%s%z')
+        end
+      end
+      class Date
+        def as_json(options = nil)
+          strftime('%Y-%m-%dT%H:%M:%S.%s%z')
+        end
+      end
+      class Time
+        def as_json(options = nil)
+          strftime('%Y-%m-%dT%H:%M:%S.%s%z')
+        end
+      end
+    end
   end
 end
