89542342e1d581e46ef539509067b1754f1096c9
Kristian Meier <m.kristian@web.de>
2011-07-13 19:08:01 +0530

    handles Location header as well

--- resty-generators/lib/resty/child_path.rb
+++ resty-generators/lib/resty/child_path.rb
@@ -6,15 +6,26 @@ module Resty
     end
 
     def call(env)
+      is_json = (env['CONTENT_TYPE'] || env['HTTP_ACCEPT']) =~ /^application\/json/
+      is_child = false
       ['REQUEST_PATH','PATH_INFO','REQUEST_URI','SCRIPT_NAME'].each do |key|
-        if(env[key] =~ /\.json([?].*)?$/)
-          env[key].gsub!(/^\/#{@rootpath}\//, "/")
-          status, headers, response = @app.call(env)
-          headers['Content-Type'] = 'application/javascript'
-          return [status, headers, response]
+        if(is_json || env[key] =~ /\.json([?].*)?$/)
+          is_json = true
+          value = env[key]
+          if value
+            is_child = is_child || value =~ /^\/#{@rootpath}\//
+            value.gsub!(/^\/#{@rootpath}\//, "/")
+          end
         end
       end
-      @app.call(env)
+      status, headers, response = @app.call(env)
+      if is_child && headers['Location']
+        uri = URI(headers['Location']) 
+        uri.path.gsub!(/^\//, "/#{@rootpath}/")
+        headers['Location'] = uri.to_s
+      end
+      headers['Content-Type'] = 'application/json' if is_json
+      [status, headers, response]
     end
   end
 end
