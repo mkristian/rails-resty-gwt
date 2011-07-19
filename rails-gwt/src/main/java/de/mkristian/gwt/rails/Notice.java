9d1b5f2f17d7fea476f584e8d3c138cbf977ba8c
Kristian Meier <m.kristian@web.de>
2011-07-19 09:53:13 +0530

    made it a popup panel

--- rails-gwt/src/main/java/de/mkristian/gwt/rails/Notice.java
+++ rails-gwt/src/main/java/de/mkristian/gwt/rails/Notice.java
@@ -4,16 +4,28 @@
 package de.mkristian.gwt.rails;
 
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.inject.Singleton;
 
 @Singleton
-public class Notice extends Label {
+public class Notice extends PopupPanel {
+    Label notice = new Label();
+    
 	public Notice(){
 		setStyleName("notice");
+		setWidget(notice);
+        setVisible(false);
 	}
 	
 	public void setText(String msg){
-		super.setText(msg);
-		setVisible(msg != null);
+		notice.setText(msg);
+		if(msg == null){
+            setVisible(false);
+		    hide();
+		}
+		else {
+            setVisible(true);
+		    show();
+		}
 	}
 }
