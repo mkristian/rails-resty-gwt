ceb205ad6e3587b10e028de25d5fef38cb966017
Kristian Meier <m.kristian@web.de>
2011-10-29 23:07:01 +0530

    reworked calculation of query, token, resourceName

--- rails-gwt/src/main/java/de/mkristian/gwt/rails/places/RestfulPlaceHistoryMapper.java
+++ rails-gwt/src/main/java/de/mkristian/gwt/rails/places/RestfulPlaceHistoryMapper.java
@@ -8,11 +8,10 @@ import java.util.Map;
 
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceHistoryMapper;
-
+import static de.mkristian.gwt.rails.places.RestfulPlaceTokenizer.SEPARATOR;
+import static de.mkristian.gwt.rails.places.QueryableRestfulPlaceTokenizer.QUERY_SEPARATOR;
 
 public class RestfulPlaceHistoryMapper implements PlaceHistoryMapper {
-
-    private static final String SEPARATOR = RestfulPlaceTokenizer.SEPARATOR;
 
     private final Map<String, RestfulPlaceTokenizer<?>> map = new HashMap<String, RestfulPlaceTokenizer<?>>();
 
@@ -21,20 +20,36 @@ public class RestfulPlaceHistoryMapper implements PlaceHistoryMapper {
     }
 
     public Place getPlace(String token) {
-        if (!token.endsWith(SEPARATOR)) {
-            token += SEPARATOR;
+        // name => ""
+        // name/ => ""
+        // name$query => $query
+        // name/something => "something"
+        // name/something$query => something$query
+        final String key;
+        final String subtoken;
+        int queryStart = token.indexOf(QUERY_SEPARATOR);
+        if(!token.contains(SEPARATOR)){
+            if(queryStart > 0){
+                key = token.substring(0, queryStart);
+            }
+            else {
+                key = token;
+            }
+            subtoken = token.substring(key.length());
         }
-        int index = token.indexOf(SEPARATOR);
-        RestfulPlaceTokenizer<?> tokenizer = this.map.get(token.substring(0,
-                index));
+        else {
+            key = token.substring(0, token.indexOf(SEPARATOR));
+            subtoken = token.substring(key.length() + 1);
+        }
+        RestfulPlaceTokenizer<?> tokenizer = this.map.get(key);
         if (tokenizer == null) {
             return null;
         } else {
-            return tokenizer.getPlace(token.substring(index + 1));
+            return tokenizer.getPlace(subtoken);
         }
     }
 
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings({"rawtypes", "unchecked"})
     public String getToken(Place place) {
         RestfulPlaceTokenizer tokenizer = this.map
                 .get(((RestfulPlace) place).resourceName);
