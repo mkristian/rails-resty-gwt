5ab3952dde575a8a87b16f68d5ffae0a203f1eaf
Kristian Meier <m.kristian@web.de>
2011-10-29 23:00:42 +0530

    pass setName through

--- rails-gwt/src/main/java/de/mkristian/gwt/rails/editors/IdentifyableListBox.java
+++ rails-gwt/src/main/java/de/mkristian/gwt/rails/editors/IdentifyableListBox.java
@@ -4,7 +4,6 @@
 package de.mkristian.gwt.rails.editors;
 
 import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.ValueListBox;
 
 import de.mkristian.gwt.rails.models.HasToDisplay;
 import de.mkristian.gwt.rails.models.Identifyable;
@@ -17,5 +16,9 @@ public class IdentifyableListBox<T extends Identifyable & HasToDisplay> extends 
     
     public void setEnabled(boolean enabled){
         ((ListBox) getWidget()).setEnabled(enabled);
+    }
+    
+    public void setName(String name){
+        ((ListBox) getWidget()).setName(name); 
     }
 }
