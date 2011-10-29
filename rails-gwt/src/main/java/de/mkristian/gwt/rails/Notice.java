b4063c0a6f74832b847af6ea214d0be65956abfd
Kristian Meier <m.kristian@web.de>
2011-10-29 22:58:31 +0530

    moved to info, warn, error methods

--- rails-gwt/src/main/java/de/mkristian/gwt/rails/Notice.java
+++ rails-gwt/src/main/java/de/mkristian/gwt/rails/Notice.java
@@ -3,29 +3,74 @@
  */
 package de.mkristian.gwt.rails;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.inject.Singleton;
 
 @Singleton
-public class Notice extends PopupPanel {
-    Label notice = new Label();
+public class Notice extends FlowPanel {
+    
+    private final Label notice = new Label();
+    private final Label loading = new Label("loading");
+    
+    private final PopupPanel popup = new PopupPanel(true);
+    
+    public Notice() {
+        loading.setStyleName("gwt-rails-loading");
+        loading.setVisible(false);
+        popup.setStyleName("gwt-rails-info");
+        popup.setWidget(notice);
+        popup.hide();
+        popup.setVisible(false);
+        add(loading);
+    }
 
- public Notice(){
-  setStyleName("notice");
-  setWidget(notice);
-        setVisible(false);
- }
+    @Deprecated
+    public void setText(String msg){
+        GWT.log("DEPRECATED: use error, info and warn instead");
+        show(msg);
+    }
 
- public void setText(String msg){
-  notice.setText(msg);
-  if(msg == null){
-            setVisible(false);
-      hide();
-  }
-  else {
-            setVisible(true);
-      show();
-  }
- }
+    private void show(String msg) {
+        popup.setVisible(true);
+        notice.setText(msg);
+        if (msg == null) {
+            popup.hide();
+        } else {
+            popup.show();
+        }
+    }
+    
+    public void info(String msg){
+        show(msg);
+        popup.setStyleName("gwt-rails-info");
+    }
+
+    public void warn(String msg){
+        show(msg);
+        popup.setStyleName("gwt-rails-warn");
+    }
+    
+    public void error(String msg){
+        show(msg);
+        popup.setStyleName("gwt-rails-error");
+    }
+
+    public void error(String msg, Throwable e){
+        error(msg + " - " + e.getMessage());
+    }
+
+    public void hide(){
+        popup.hide();
+    }
+    
+    public void loading(){
+        loading.setVisible(true);
+    }
+
+    public void finishLoading(){
+        loading.setVisible(false);
+    }
 }
