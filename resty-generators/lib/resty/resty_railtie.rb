27f688ad3875dc4cd28722a9d35c93bb92e07c25
Kristian Meier <m.kristian@web.de>
2012-07-04 09:42:05 +0530

    more date/time format hacks and allow rails3.2.x on railties

--- resty-generators/lib/resty/resty_railtie.rb
+++ resty-generators/lib/resty/resty_railtie.rb
@@ -1,9 +1,33 @@
 require 'rails'
 
+# TODO decide where to put it here or in after_initialize
+# DateTime and Time do not work - no usec
+class Date
+  def as_json(options = nil)
+    strftime('%Y-%m-%dT0:0:0.000')
+  end
+end
+# class DateTime
+#   def as_json(options = nil)
+#     strftime('%Y-%m-%dT%H:%M:%S.') + ("%06d" % usec) + strftime('%z')
+#   end
+# end
+class ActiveSupport::TimeWithZone
+  def as_json(options = nil)
+    strftime('%Y-%m-%dT%H:%M:%S.') + ("%06d" % usec) + strftime('%z')
+  end
+end
+# class Time
+#   def as_json(options = nil)
+#     strftime('%Y-%m-%dT%H:%M:%S.') + ("%06d" % usec) + strftime('%z')
+#   end
+# end
+
 module Resty
   class RestyRailtie < Rails::Railtie
 
-    config.generators do
+    gmethod = config.respond_to?(:generators) ? :generators : :app_generators
+    config.send(gmethod) do
       require 'rails/generators'      
       require 'rails/generators/rails/controller/controller_generator'
       #require 'rails/generators/erb/scaffold/scaffold_generator'
@@ -14,6 +38,11 @@ module Resty
 
     config.after_initialize do
       # get the time/date format right ;-) and match it with resty
+      class Date
+        def as_json(options = nil)
+          strftime('%Y-%m-%dT0:0:0.000%z')
+        end
+      end
       class DateTime
         def as_json(options = nil)
           strftime('%Y-%m-%dT%H:%M:%S.') + ("%06d" % usec) + strftime('%z')
